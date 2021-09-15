7e41775e44628b86dd12b750c134770a678d1908
stn1slv <devyatov@gmail.com>
2021-09-15 20:07:55 +0300

    confluent-kafka-go was replaced by sarama

--- go.mod
+++ go.mod
@@ -3,16 +3,29 @@ module github.com/stn1slv/kafka-console-avro-tools
 go 1.17
 
 require (
-	github.com/google/uuid v1.3.0
+	github.com/Shopify/sarama v1.29.1
 	github.com/jessevdk/go-flags v1.5.0
 	github.com/riferrei/srclient v0.4.0
-	gopkg.in/confluentinc/confluent-kafka-go.v1 v1.7.0
 )
 
 require (
-	github.com/confluentinc/confluent-kafka-go v1.7.0 // indirect
-	github.com/golang/snappy v0.0.1 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/eapache/go-resiliency v1.2.0 // indirect
+	github.com/eapache/go-xerial-snappy v0.0.0-20180814174437-776d5712da21 // indirect
+	github.com/eapache/queue v1.1.0 // indirect
+	github.com/golang/snappy v0.0.3 // indirect
+	github.com/hashicorp/go-uuid v1.0.2 // indirect
+	github.com/jcmturner/aescts/v2 v2.0.0 // indirect
+	github.com/jcmturner/dnsutils/v2 v2.0.0 // indirect
+	github.com/jcmturner/gofork v1.0.0 // indirect
+	github.com/jcmturner/gokrb5/v8 v8.4.2 // indirect
+	github.com/jcmturner/rpc/v2 v2.0.3 // indirect
+	github.com/klauspost/compress v1.12.2 // indirect
 	github.com/linkedin/goavro/v2 v2.9.7 // indirect
+	github.com/pierrec/lz4 v2.6.0+incompatible // indirect
+	github.com/rcrowley/go-metrics v0.0.0-20201227073835-cf1acfcdf475 // indirect
+	golang.org/x/crypto v0.0.0-20210616213533-5ff15b29337e // indirect
+	golang.org/x/net v0.0.0-20210614182718-04defd469f4e // indirect
 	golang.org/x/sync v0.0.0-20201020160332-67f06af15bc9 // indirect
-	golang.org/x/sys v0.0.0-20210320140829-1e4c9ba3b0c4 // indirect
+	golang.org/x/sys v0.0.0-20210615035016-665e8c7367d1 // indirect
 )
--- main.go
+++ main.go
@@ -1,15 +1,18 @@
 package main
 
 import (
+	"context"
 	"encoding/binary"
 	"fmt"
 	"log"
 	"os"
+	"os/signal"
+	"sync"
+	"syscall"
 
-	"github.com/google/uuid"
+	sarama "github.com/Shopify/sarama"
 	flags "github.com/jessevdk/go-flags"
 	"github.com/riferrei/srclient"
-	"gopkg.in/confluentinc/confluent-kafka-go.v1/kafka"
 )
 
 var opts struct {
@@ -19,6 +22,7 @@ var opts struct {
 	SchemaID          int    `long:"schemaId" env:"SCHEMA_ID" description:"Schema ID"`
 	SchemaRegistryURL string `long:"schemaRegistryURL" env:"SCHEMA_REGISTRY_URL" default:"http://localhost:8081" description:"Schema Registry URL"`
 	Message           string `short:"m" long:"msg" env:"MSG" description:"Message"`
+	Group             string `short:"g" long:"group" env:"GROUP" default:"kafka-console-avro-tools" description:"Consumer group"`
 }
 
 func failOnError(err error, msg string) {
@@ -33,28 +37,23 @@ func main() {
 		os.Exit(1)
 	}
 
+	brokers := []string{opts.BrokerList}
+
+	config := sarama.NewConfig()
+	config.Producer.RequiredAcks = sarama.WaitForAll
+	config.Producer.Retry.Max = 5
+	config.Producer.Return.Successes = true
+	config.ClientID = "go-kafka-consumer"
+	config.Consumer.Return.Errors = true
+	schemaRegistryClient := srclient.CreateSchemaRegistryClient(opts.SchemaRegistryURL)
+
 	if opts.Mode == "producer" {
 		// 1) Create the producer as you would normally do using Confluent's Go client
-		p, err := kafka.NewProducer(&kafka.ConfigMap{"bootstrap.servers": opts.BrokerList})
-		failOnError(err, "Error to connect to kafka")
+		p, err := sarama.NewSyncProducer(brokers, config)
+		failOnError(err, "Error connecting to kafka")
 		defer p.Close()
 
-		go func() {
-			for event := range p.Events() {
-				switch ev := event.(type) {
-				case *kafka.Message:
-					message := ev
-					if ev.TopicPartition.Error != nil {
-						fmt.Printf("Error delivering the message '%s'\n", message.Key)
-					} else {
-						fmt.Printf("Message '%s' delivered successfully!\n", message.Key)
-					}
-				}
-			}
-		}()
-
 		// 2) Fetch schema definition
-		schemaRegistryClient := srclient.CreateSchemaRegistryClient(opts.SchemaRegistryURL)
 		schema, err := schemaRegistryClient.GetSchema(opts.SchemaID)
 		failOnError(err, "Error getting Avro schema from Confluent Schema Registry")
 		schemaIDBytes := make([]byte, 4)
@@ -73,49 +72,107 @@ func main() {
 		recordValue = append(recordValue, schemaIDBytes...)
 		recordValue = append(recordValue, valueBytes...)
 
-		key, _ := uuid.NewUUID()
-		p.Produce(&kafka.Message{
-			TopicPartition: kafka.TopicPartition{
-				Topic: &opts.Topic, Partition: kafka.PartitionAny},
-			Key: []byte(key.String()), Value: recordValue}, nil)
-
-		p.Flush(15 * 1000)
-	} else if opts.Mode == "consumer" {
-		// 1) Create the consumer as you would normally do using Confluent's Go client
-		c, err := kafka.NewConsumer(&kafka.ConfigMap{
-			"bootstrap.servers": opts.BrokerList,
-			"group.id":          "myGroup",
-			"auto.offset.reset": "earliest",
+		// key, _ := uuid.NewUUID()
+		partition, offset, err := p.SendMessage(&sarama.ProducerMessage{
+			Topic: opts.Topic,
+			Value: sarama.ByteEncoder(recordValue),
 		})
-		failOnError(err, "Error to connect to kafka")
-		defer c.Close()
-		c.SubscribeTopics([]string{opts.Topic}, nil)
+		failOnError(err, "Failed to send message")
 
-		// 2) Create a instance of the client to retrieve the schemas for each message
-		schemaRegistryClient := srclient.CreateSchemaRegistryClient(opts.SchemaRegistryURL)
-
-		for {
-			msg, err := c.ReadMessage(-1)
-			if err == nil {
-				// 3) Recover the schema id from the message and use the
-				// client to retrieve the schema from Schema Registry.
-				// Then use it to deserialize the record accordingly.
-				schemaID := binary.BigEndian.Uint32(msg.Value[1:5])
-				schema, err := schemaRegistryClient.GetSchema(int(schemaID))
-				failOnError(err, fmt.Sprintf("Error getting the schema with id '%d' %s", schemaID, err))
-
-				native, _, err := schema.Codec().NativeFromBinary(msg.Value[5:])
-				failOnError(err, "NativeFromBinary error")
-
-				value, err := schema.Codec().TextualFromNative(nil, native)
-				failOnError(err, "TextualFromNative error")
-				fmt.Printf("Received: %s\n", string(value))
-			} else {
-				failOnError(err, "Error consuming the message")
-			}
+		log.Printf("Wrote message at partition: %d, offset: %d\n", partition, offset)
+	} else if opts.Mode == "consumer" {
+		/**
+		 * Setup a new Sarama consumer group
+		 */
+		consumer := Consumer{
+			ready:    make(chan bool),
+			srClient: schemaRegistryClient,
 		}
 
+		ctx, cancel := context.WithCancel(context.Background())
+		client, err := sarama.NewConsumerGroup(brokers, opts.Group, config)
+		if err != nil {
+			log.Panicf("Error creating consumer group client: %v", err)
+		}
+
+		wg := &sync.WaitGroup{}
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			for {
+				// `Consume` should be called inside an infinite loop, when a
+				// server-side rebalance happens, the consumer session will need to be
+				// recreated to get the new claims
+				if err := client.Consume(ctx, []string{opts.Topic}, &consumer); err != nil {
+					log.Panicf("Error from consumer: %v", err)
+				}
+				// check if context was cancelled, signaling that the consumer should stop
+				if ctx.Err() != nil {
+					return
+				}
+				consumer.ready = make(chan bool)
+			}
+		}()
+
+		<-consumer.ready // Await till the consumer has been set up
+		log.Println("Consumer up and running!...")
+
+		sigterm := make(chan os.Signal, 1)
+		signal.Notify(sigterm, syscall.SIGINT, syscall.SIGTERM)
+		select {
+		case <-ctx.Done():
+			log.Println("terminating: context cancelled")
+		case <-sigterm:
+			log.Println("terminating: via signal")
+		}
+		cancel()
+		wg.Wait()
+		if err = client.Close(); err != nil {
+			log.Panicf("Error closing client: %v", err)
+		}
 	} else {
 		log.Fatalf("Invalid mode. Please specify producer or consumer")
 	}
+}
+
+// Consumer represents a Sarama consumer group consumer
+type Consumer struct {
+	ready    chan bool
+	srClient *srclient.SchemaRegistryClient
+}
+
+// Setup is run at the beginning of a new session, before ConsumeClaim
+func (consumer *Consumer) Setup(sarama.ConsumerGroupSession) error {
+	// Mark the consumer as ready
+	close(consumer.ready)
+	return nil
+}
+
+// Cleanup is run at the end of a session, once all ConsumeClaim goroutines have exited
+func (consumer *Consumer) Cleanup(sarama.ConsumerGroupSession) error {
+	return nil
+}
+
+// ConsumeClaim must start a consumer loop of ConsumerGroupClaim's Messages().
+func (consumer *Consumer) ConsumeClaim(session sarama.ConsumerGroupSession, claim sarama.ConsumerGroupClaim) error {
+	// NOTE:
+	// Do not move the code below to a goroutine.
+	// The `ConsumeClaim` itself is called within a goroutine, see:
+	// https://github.com/Shopify/sarama/blob/main/consumer_group.go#L27-L29
+	for message := range claim.Messages() {
+
+		schemaID := binary.BigEndian.Uint32(message.Value[1:5])
+		schema, err := consumer.srClient.GetSchema(int(schemaID))
+		failOnError(err, fmt.Sprintf("Error getting the schema with id '%d' %s", schemaID, err))
+		native, _, err := schema.Codec().NativeFromBinary(message.Value[5:])
+		failOnError(err, "NativeFromBinary error")
+
+		value, err := schema.Codec().TextualFromNative(nil, native)
+		failOnError(err, "TextualFromNative error")
+		fmt.Printf("Received: %s\n", string(value))
+
+		session.MarkMessage(message, "")
+	}
+
+	return nil
 }
