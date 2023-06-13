7a46e378d3574e18b89c487972c5961788c442eb
Stanislav Deviatov <devyatov@gmail.com>
2023-06-13 15:07:52 +0200

    Upgrade to 1.20 golang version (#6)

--- tls.go
+++ tls.go
@@ -3,29 +3,33 @@ package main
 import (
 	"crypto/tls"
 	"crypto/x509"
-	"io/ioutil"
+	"fmt"
+	"os"
 )
 
 // NewTLSConfig creates a new TLS configuration for the given certificate files.
 func NewTLSConfig(clientCertFile, clientKeyFile, caCertFile string) (*tls.Config, error) {
-	tlsConfig := tls.Config{}
+	var tlsConfig = &tls.Config{}
 
 	// Load client cert
 	cert, err := tls.LoadX509KeyPair(clientCertFile, clientKeyFile)
 	if err != nil {
-		return &tlsConfig, err
+		return nil, fmt.Errorf("failed to load client cert: %w", err)
 	}
 	tlsConfig.Certificates = []tls.Certificate{cert}
 
 	// Load CA cert
-	caCert, err := ioutil.ReadFile(caCertFile)
+	caCert, err := os.ReadFile(caCertFile)
 	if err != nil {
-		return &tlsConfig, err
+		return nil, fmt.Errorf("failed to read CA cert file: %w", err)
 	}
+
 	caCertPool := x509.NewCertPool()
-	caCertPool.AppendCertsFromPEM(caCert)
+	if ok := caCertPool.AppendCertsFromPEM(caCert); !ok {
+		return nil, fmt.Errorf("failed to append CA cert")
+	}
+
 	tlsConfig.RootCAs = caCertPool
 
-	tlsConfig.BuildNameToCertificate()
-	return &tlsConfig, err
+	return tlsConfig, nil
 }
